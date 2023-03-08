b44a31b4d26aeafc4f35af0f7c3721b66225ff12
subzero <shubham8111994@gmail.com>
2023-03-08 12:33:59 +0530

    Added: program for bubble sort

---
+++ src/Bubblesort/BubbleSort.java
@@ -0,0 +1,29 @@
+package Bubblesort;
+
+// Program to sort using bubble sort
+public class BubbleSort {
+    public static void bubbleSort(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length-i-1; j++) {
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        for (int key:array) {
+            System.out.print(key+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] array={2,1,4,3,7,3};
+        System.out.println("array");
+        for (int i:array) {
+            System.out.print(i+" ");
+        }
+        System.out.println("\nSorted array is");
+        bubbleSort(array);
+
+    }
+}
